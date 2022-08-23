45f4f9e28fd7d709c8644f100aa253394d1d93ee
nathanielmatega <103465215+nathanielmatega@users.noreply.github.com>
2022-08-24 11:52:57 +1200

    Add files via upload

---
+++ lesson1/basic_var_and_output.py
@@ -0,0 +1,14 @@
+# Nathaniel Matega
+#  Varibles 
+
+# declaration of variable\
+name = "elephant"
+numLegs = 4
+height = 4.5
+hastail = True
+
+# Output to screen
+print(name)
+print(numLegs)
+print(height)
+print(hastail)
