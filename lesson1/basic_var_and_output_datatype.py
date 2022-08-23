8b2f5cfc2a418f58daddd4324f53fc65c56056a6
nathanielmatega <103465215+nathanielmatega@users.noreply.github.com>
2022-08-24 11:55:28 +1200

    Add files via upload

---
+++ lesson1/basic_var_and_output_datatype.py
@@ -0,0 +1,14 @@
+# Nathaniel Matega
+#  Varibles 
+
+# declaration of variable\
+name = "elephant"
+numLegs = 4
+height = 4.5
+hastail = True
+
+# output to screen
+print(type(name))
+print(type(numLegs))
+print(type(height))
+print(type(hastail))
---
+++ lesson1/basic_var_and_output_with_labels.py
@@ -0,0 +1,14 @@
+# Nathaniel Matega
+#  Varibles 
+
+# declaration of variable\
+name = "elephant"
+numLegs = 4
+height = 4.5
+hastail = True
+
+# output to screen
+print(":One of the big five animals in Aficra is called and",name,'.')
+print("AN" ,name,"has" ,numLegs)
+print(height)
+print(hastail)
