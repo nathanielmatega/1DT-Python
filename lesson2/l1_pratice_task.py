36f2d89026ca35743dffa1f238407f88996b1462
nathanielmatega <103465215+nathanielmatega@users.noreply.github.com>
2022-08-24 12:29:43 +1200

    Add files via upload

---
+++ lesson2/l1_pratice_task.py
@@ -0,0 +1,15 @@
+# Nathaniel Matega
+# Focus - Varibles, print, Labels, comments 
+
+# Variable
+name = "N junior Faatauvaa Matega"
+Address = "170 coutts street"
+height = 172.72
+phone = "02109041861"
+allergy = False
+
+print("My full name is",name,'.')
+print("I live in" ,Address,)
+print("I am ",height,"tall")
+print("my contacts are",phone,)
+print("peopls say i have an allergy but that is",allergy,)
